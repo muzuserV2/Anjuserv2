e72342e566027d14c777a8eeeca8809f4be67367
Joshuva-ser <96343347+Joshuva-ser@users.noreply.github.com>
2022-02-05 16:39:15 +0530

    Create cool.js

---
+++ plugins/cool.js
@@ -0,0 +1,23 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
+
+Asena.addCommand({pattern: 'owner', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
+
+    var skl = await axios.get("https://imgur.com/a/Frcrz99.jpeg", { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Bot owner Joshuva💞*
+
+*Creator number : http://wa.me/918129900380*
+
+
+ *ᴊᴏꜱʜᴜᴠᴀꜱᴇʀ ʙᴏᴛ: https://chat.whatsapp.com/DG87G5A3cEz4hLTH5LKbPL
+
+
+ *ᴊᴏꜱʜᴜᴠᴀꜱᴇʀꫂ⁩..♡︎*
+`}) 
+
+}));
